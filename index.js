28e29ed40514cf9e2c7ea16aaa6814e577554b8e
Dov Azogui <dov.azogui@gmail.com>
2024-08-25 21:18:35 -0400

    created 3 functions

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if(feet <= 400 ){
+    return "This one is on me!"
+  }
+
+  else if( feet <= 2000){
+    return "That will be twenty bucks."
+
+  } else if (feet <= 2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (feet >= 2500){
+    return 'No can do.'
+  }
+  }
+
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  if (city === 'NYC'){
+    return 'Ok, sounds good.'
+  }
+  else {
+    return 'No go.'
+  }
+
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(status){
   // Write your code here!
-}
+  if (status === 'generous') {
+    return 'Thank you so much.'
+  }
+  else if (status === 'not as generous'){
+    return 'Thank you.'
+  }
+  else {
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+    return "Bye."
+  }
+
 }
